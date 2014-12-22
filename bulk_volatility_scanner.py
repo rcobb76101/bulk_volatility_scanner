fb315343de09672c31369185f5e8ec307aef536b
rcobb76101 <ryancobb@gmail.com>
2014-12-21 21:52:21 -0500

    Update bulk_volatility_scanner.py
    Major updates to program structure and features. 
    
    Automatically identifies Volatility profile and selects appropriate plugins without the need for a plugin file.

--- bulk_volatility_scanner.py
+++ bulk_volatility_scanner.py
@@ -4,54 +4,134 @@
 import subprocess
 import argparse
 import os.path
+import re
+
+def ReadPlugins(plugin_file):
+    print '[Info] The following plugins will be processed against the target image file:'
+    try:
+        with open(plugin_file, 'r') as plugins:
+            print plugins.read()
+    except:
+        print '[Error] Error Reading Plugin File!'
+
+def CheckOutput(output_directory):
+    try:
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+    except:
+        print '[Error] Error Creating Output Directory: {0}'.format(output_directory)
+        
+def ProcessImage(image_file, profile, plugins, output_directory):
+    basename_imagefile  = os.path.basename(image_file)
+    
+    for plugin in plugins:
+        output_filename = plugin + '_' +  basename_imagefile
+        output_path = os.path.join(output_directory, output_filename)
+        
+        print '[Plugin] Running Plugin: {0}'.format(plugin)
+        try:
+            with open(output_path, 'w') as output:
+                subprocess.call(['vol.py', '-f', image_file, '--profile=' + profile, plugin], stdout=output)
+            print '[Plugin Completed] {0} ouput saved to {1}'.format(plugin, output_path)
+        except:
+            '[Plugin Error] Error Running Plugin: {0}'.format(plugin)
+    
+    print '[Success] Exiting Gracefully'
+   
+def IdentifyProfile(image_file):
+    print '[!] Identifying image {0}...'.format(image_file)
+    
+    data = subprocess.check_output(['vol.py', '-f', image_file, 'imageinfo'])
+    profiles_regex = re.search('Suggested Profile\(s\) : ([^\n]*)',  data)
+    profiles = profiles_regex.group(1).split(', ')
+    return profiles
+
+def SelectValidPlugins(profiles):
+    all =   ['pslist',
+                'pstree',
+             	'psscan',
+            	'dlllist',
+            	'handles',
+            	'getsids',
+            	'cmdscan',
+            	'consoles',
+            	'privs',
+            	'envars',
+            	'verinfo',
+            	'enumfunc',
+            	'memmap',
+            	'vadinfo',
+            	'vadwalk',
+            	'vadtree',
+            	'iehistory',
+            	'modules',
+            	'modscan',
+                'ssdt',
+              	'driverscan',
+            	'filescan',
+            	'mutantscan',
+            	'symlinkscan',
+            	'thrdscan',
+            	'unloadedmodules',
+            	'hivescan',
+            	'hivelist',
+            	'mftparser']
+    xp2003 = ['evtlogs',
+                'connections',
+                'connscan',
+                'sockets',
+                'sockscan']
+    vista2008win7 = ['netscan',
+                'userassist',
+                'shellbags',
+                'shimcache',
+                'getservicesids']
+                    	
+    OSType = re.match('(WinXP)|(Win2003)', profiles)
+    
+    print '[Info] The following plugins will be processed against the target image file:'
+    
+    if OSType is not None:
+        plugins = all + xp2003
+        
+        for plugin in plugins:
+            print '[Loaded Plugin] {0}'.format(plugin)
+            
+        return plugins
+    else:
+        plugins = all + vista2008win7
+        
+        for plugin in plugins:
+            print '[Loaded Plugin] {0}'.format(plugin)
+            
+        return plugins
+                
 
 #argparse module allows the user to provide -h or --help for help messages
-parser = argparse.ArgumentParser(description="Subject a target memory image to a battery of volatility plugins.", epilog="The plugin file contains line-delimited Volatility plugin commands.")
-parser.add_argument("imagefile", help="Memory image")
-parser.add_argument("profile", help="Volatility profile")
-parser.add_argument("pluginfile", help="Plugin file")
+parser = argparse.ArgumentParser(description='Run all available Volatility plugins on a target image file.',
+    epilog='''The first suggested profile will be automatically selected.
+        All available plugins will be selected for the suggested profile.
+        If the output directory does not exist, it will be created.
+        The output files with follow a $plugin_$filename format.''')
+parser.add_argument("imagefile", help="Path to Memory Image")
+parser.add_argument("outputdirectory", help="Path to Output Direcctory")
 args = parser.parse_args()
 
-#assign arguments to variables
-#image_file is the target memory image
-#profile is the volatility memory profile
-#plugin_file is a line-delimited unix text file containing volatility plugin names
-image_file = args.imagefile
-profile = args.profile
-plugin_file = args.pluginfile
+image_file          = os.path.abspath(args.imagefile)
+output_directory    = os.path.abspath(args.outputdirectory)
 
-#welcome message
-print "Bulk Volatility Scanner v0.0.1"
-print "by Ryan Cobb 1/12/2014\n"
-print "Target Image File is %s" % image_file
-print "Plugin Configuration File is %s" % plugin_file
-print "The following plugins will be processed against the target image file."
+print 'Bulk Volatility Scanner v0.3 - Ryan Cobb 12/20/2014'
+print '[Info] Target Image File: {0}'.format(image_file)
 
-#lists all plugins from inside the plugin file
-plugin_list = open(plugin_file)
-print plugin_list.read()
+profiles = IdentifyProfile(image_file)
+#profiles            = ['Win2008R2SP0x64', 'Win2008RSP1x64']
 
-#starts execution of plugins
-print "Executing plugin battery on %s" % image_file
-try:
+print '[Info] Selected Memory Profile: {0}'.format(profiles[0])
 
-#sets and creates directory filename based on image filename
-        directory_filename = image_file + "_" + "output"
-        os.mkdir(directory_filename)
+plugins = SelectValidPlugins(profiles[0])
 
-#primary execution loop for plugins
-        for plugin in open(plugin_file):
-                try:
-                        print "Plugin: " + plugin.rstrip() + " in progress"
-                        output_filename = plugin.rstrip() + ".txt"
-                        output_file = open(os.path.join(directory_filename, output_filename), "w")
-                        subprocess.call(["vol.py", "-f", image_file, "--profile="+profile, plugin.rstrip()], stdout=output_file)
-                        print "Plugin: " + plugin.rstrip() + " completed"
-                        print "Output saved to %s/%s" % (directory_filename, output_filename)
-                        print "\n"
-                except:
-                        print 'Incompatible plugin? Check your profile.'
+#ReadPlugins(plugin_file)
 
-        print "Bulk processing complete. Exiting gracefully."
-except:
-        print "An error has occured. Exiting."
+CheckOutput(output_directory)
+
+ProcessImage(image_file, profiles[0], plugins, output_directory)
