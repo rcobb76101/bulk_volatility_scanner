1c37a66ffffcc2582f0894e4d7f5eb26be010901
rcobb76101 <ryancobb@gmail.com>
2016-07-09 23:13:54 -0400

    Update bulk_volatility_scanner.py
    Fixed indent

--- bulk_volatility_scanner.py
+++ bulk_volatility_scanner.py
@@ -5,115 +5,114 @@ import re
 import multiprocessing
 import subprocess
 import sys
-import signal
 import time
 
 logging.basicConfig(level=logging.DEBUG, 
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-def init_worker():
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
+ALL_PROFILES = [
+	'VistaSP0x64',
+	'VistaSP0x86',
+	'VistaSP1x64',
+	'VistaSP1x86',
+	'VistaSP2x64',
+	'VistaSP2x86',
+	'Win10x64',
+	'Win10x86',
+	'Win2003SP0x86',
+	'Win2003SP1x64',
+	'Win2003SP1x86',
+	'Win2003SP2x64',
+	'Win2003SP2x86',
+	'Win2008R2SP0x64',
+	'Win2008R2SP1x64',
+	'Win2008SP1x64',
+	'Win2008SP1x86',
+	'Win2008SP2x64',
+	'Win2008SP2x86',
+	'Win2012R2x64',
+	'Win2012x64',
+	'Win7SP0x64',
+	'Win7SP0x86',
+	'Win7SP1x64',
+	'Win7SP1x86',
+	'Win81U1x64',
+	'Win81U1x86',
+	'Win8SP0x64',
+	'Win8SP0x86',
+	'Win8SP1x64',
+	'Win8SP1x86',
+	'WinXPSP1x64',
+	'WinXPSP2x64',
+	'WinXPSP2x86',
+	'WinXPSP3x86']
 
-ALL_PROFILES = ['VistaSP0x64',
-				'VistaSP0x86',
-				'VistaSP1x64',
-				'VistaSP1x86',
-				'VistaSP2x64',
-				'VistaSP2x86',
-				'Win10x64',
-				'Win10x86',
-				'Win2003SP0x86',
-				'Win2003SP1x64',
-				'Win2003SP1x86',
-				'Win2003SP2x64',
-				'Win2003SP2x86',
-				'Win2008R2SP0x64',
-				'Win2008R2SP1x64',
-				'Win2008SP1x64',
-				'Win2008SP1x86',
-				'Win2008SP2x64',
-				'Win2008SP2x86',
-				'Win2012R2x64',
-				'Win2012x64',
-				'Win7SP0x64',
-				'Win7SP0x86',
-				'Win7SP1x64',
-				'Win7SP1x86',
-				'Win81U1x64',
-				'Win81U1x86',
-				'Win8SP0x64',
-				'Win8SP0x86',
-				'Win8SP1x64',
-				'Win8SP1x86',
-				'WinXPSP1x64',
-				'WinXPSP2x64',
-				'WinXPSP2x86',
-				'WinXPSP3x86']
+BASE_PLUGINS = [
+	'pslist',
+	'pstree',
+	'psscan',
+	'dlllist',
+	'handles',
+	'getsids',
+	'cmdscan',
+	'consoles',
+	'privs',
+	'envars',
+	'verinfo',
+	'enumfunc',
+	'memmap',
+	'vadinfo',
+	'vadwalk',
+	'vadtree',
+	'iehistory',
+	'modules',
+	'modscan',
+	'ssdt',
+	'driverscan',
+	'filescan',
+	'mutantscan',
+	'symlinkscan',
+	'thrdscan',
+	'unloadedmodules',
+	'hivescan',
+	'hivelist',
+	'psxview',
+	'malfind',
+	'sessions',
+	'wndscan',
+	'deskscan',
+	'atomscan',
+	'clipboard',
+	'eventhooks',
+	'gahti',
+	'messagehooks',
+	'userhandles',
+	'gditimers',
+	'windows',
+	'wintree',
+	'svcscan',
+	'ldrmodules',
+	'apihooks',
+	'idt',
+	'gdt',
+	'threads',
+	'callbacks',
+	'devicetree',
+	'timers']
 
-BASE_PLUGINS =   ['pslist',
-					'pstree',
-					'psscan',
-					'dlllist',
-					'handles',
-					'getsids',
-					'cmdscan',
-					'consoles',
-					'privs',
-					'envars',
-					'verinfo',
-					'enumfunc',
-					'memmap',
-					'vadinfo',
-					'vadwalk',
-					'vadtree',
-					'iehistory',
-					'modules',
-					'modscan',
-					'ssdt',
-					'driverscan',
-					'filescan',
-					'mutantscan',
-					'symlinkscan',
-					'thrdscan',
-					'unloadedmodules',
-					'hivescan',
-					'hivelist',
-					'psxview',
-					'malfind',
-					'sessions',
-					'wndscan',
-					'deskscan',
-					'atomscan',
-					'clipboard',
-					'eventhooks',
-					'gahti',
-					'messagehooks',
-					'userhandles',
-					'gditimers',
-					'windows',
-					'wintree',
-					'svcscan',
-					'ldrmodules',
-					'apihooks',
-					'idt',
-					'gdt',
-					'threads',
-					'callbacks',
-					'devicetree',
-					'timers']
-
-XP2003_PLUGINS = 	['evtlogs',
-					'connections',
-					'connscan',
-					'sockets',
-					'sockscan']
+XP2003_PLUGINS = [
+	'evtlogs',
+	'connections',
+	'connscan',
+	'sockets',
+	'sockscan']
 
 VISTA_WIN2008_WIN7_PLUGINS = [
-					'netscan',
-					'userassist',
-					'shellbags',
-					'shimcache',
-					'getservicesids']
+	'netscan',
+	'userassist',
+	'shellbags',
+	'shimcache',
+	'getservicesids']
 
 class MemoryImage(object):
 	def __init__(self, invocation, image_path, profile, kdbg, master_output_directory, plugins_list):
@@ -151,7 +150,6 @@ class MemoryImage(object):
 		if not self.profile:
 			self.profile = auto_profiles[0]
 		
-
 		if not self.kdbg:
 			self.kdbg = auto_kdbg
 
@@ -168,6 +166,7 @@ class MemoryImage(object):
 	
 		logging.info('[{0}] Selected Profile: {1}'.format(self.basename, self.profile))
 		logging.info('[{0}] Selected KDBG Offset: {1}'.format(self.basename, self.kdbg))
+
 		for plugin in self.valid_plugins:
 			logging.info('[{0}] Queuing plugin: {1}'.format(self.basename, plugin.strip('\n')))
 
@@ -203,7 +202,6 @@ if __name__ == '__main__':
 		invocation = 'vol.exe'
 
 	master_output_directory = os.path.abspath(args.output_directory)
-
 	if not os.path.exists(master_output_directory):
 		os.makedirs(master_output_directory)
 
@@ -231,37 +229,31 @@ if __name__ == '__main__':
 			commandline = [invocation, '-f', image.abspath, '--profile=' + profile, '--kdbg=' + image.kdbg, plugin_name]
 			commandline += plugin_flags
 
-			commands.append(
-				{
+			commands.append({
 				'image_basename': image.basename, 
 				'plugin_name': plugin_name, 
 				'commandline': commandline, 
-				'output_path': output_path
-				})
-
+				'output_path': output_path})
 
 	workers = []
-
 	while True:
 		logging.debug('Active Workers: {0}, Pending Tasks: {1}'.format(
 			len(workers), len(commands)))
 
 		if len(commands) == 0 and len(workers) == 0:
 			break
-
 		try:
 			if len(commands) > 0 and len(workers) < 4:
 				command = commands.pop()
 				worker = multiprocessing.Process(target=execute_plugin, args=(command,))
-				workers.append(
-					{'plugin_name': command['plugin_name'], 
+				workers.append({
+					'plugin_name': command['plugin_name'], 
 					'task': worker,
 					'image_basename': command['image_basename']})
 				worker.start()
 			else:
 				time.sleep(5)
 				logging.debug('Polling workers....')
-
 				for i, worker in enumerate(workers):
 					logging.debug('[{0}] Worker for {1} is still alive?: {2}'.format(
 						worker['image_basename'], worker['plugin_name'], worker['task'].is_alive()))
@@ -271,7 +263,6 @@ if __name__ == '__main__':
 						workers.pop(i)
 					else:
 						continue
-						
 		except KeyboardInterrupt:
 			for worker in workers:
 				worker['task'].terminate()
