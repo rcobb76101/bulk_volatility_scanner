e6e946b49006678f89f1d3691f0cabda8737242f
rcobb76101 <ryancobb@gmail.com>
2016-07-09 23:09:10 -0400

    Update bulk_volatility_scanner.py
    Total re-write including 2.7 through 3.5 compatibility, process pool that actually listens for signal interrupts, and more!

--- bulk_volatility_scanner.py
+++ bulk_volatility_scanner.py
@@ -1,241 +1,281 @@
-#!/usr/bin/env python
-
-#Import necessary modules
-import subprocess
 import argparse
-import os.path
+import logging
+import os
 import re
 import multiprocessing
+import subprocess
 import sys
+import signal
+import time
+
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+def init_worker():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+ALL_PROFILES = ['VistaSP0x64',
+				'VistaSP0x86',
+				'VistaSP1x64',
+				'VistaSP1x86',
+				'VistaSP2x64',
+				'VistaSP2x86',
+				'Win10x64',
+				'Win10x86',
+				'Win2003SP0x86',
+				'Win2003SP1x64',
+				'Win2003SP1x86',
+				'Win2003SP2x64',
+				'Win2003SP2x86',
+				'Win2008R2SP0x64',
+				'Win2008R2SP1x64',
+				'Win2008SP1x64',
+				'Win2008SP1x86',
+				'Win2008SP2x64',
+				'Win2008SP2x86',
+				'Win2012R2x64',
+				'Win2012x64',
+				'Win7SP0x64',
+				'Win7SP0x86',
+				'Win7SP1x64',
+				'Win7SP1x86',
+				'Win81U1x64',
+				'Win81U1x86',
+				'Win8SP0x64',
+				'Win8SP0x86',
+				'Win8SP1x64',
+				'Win8SP1x86',
+				'WinXPSP1x64',
+				'WinXPSP2x64',
+				'WinXPSP2x86',
+				'WinXPSP3x86']
+
+BASE_PLUGINS =   ['pslist',
+					'pstree',
+					'psscan',
+					'dlllist',
+					'handles',
+					'getsids',
+					'cmdscan',
+					'consoles',
+					'privs',
+					'envars',
+					'verinfo',
+					'enumfunc',
+					'memmap',
+					'vadinfo',
+					'vadwalk',
+					'vadtree',
+					'iehistory',
+					'modules',
+					'modscan',
+					'ssdt',
+					'driverscan',
+					'filescan',
+					'mutantscan',
+					'symlinkscan',
+					'thrdscan',
+					'unloadedmodules',
+					'hivescan',
+					'hivelist',
+					'psxview',
+					'malfind',
+					'sessions',
+					'wndscan',
+					'deskscan',
+					'atomscan',
+					'clipboard',
+					'eventhooks',
+					'gahti',
+					'messagehooks',
+					'userhandles',
+					'gditimers',
+					'windows',
+					'wintree',
+					'svcscan',
+					'ldrmodules',
+					'apihooks',
+					'idt',
+					'gdt',
+					'threads',
+					'callbacks',
+					'devicetree',
+					'timers']
+
+XP2003_PLUGINS = 	['evtlogs',
+					'connections',
+					'connscan',
+					'sockets',
+					'sockscan']
+
+VISTA_WIN2008_WIN7_PLUGINS = [
+					'netscan',
+					'userassist',
+					'shellbags',
+					'shimcache',
+					'getservicesids']
 
 class MemoryImage(object):
-    def __init__(self, image):
-        self.basename = os.path.basename(image)
-        self.abspath = os.path.abspath(image)
-        self.output = os.path.join(output_directory, self.basename)
-        self.kdbg = ''
-        self.profile = ''
-        
-        if not os.path.exists(self.output):
-            os.makedirs(self.output)  
-            
-        self.all_profiles = ['VistaSP0x64',
-                                'VistaSP0x86',
-                                'VistaSP1x64',
-                                'VistaSP1x86',
-                                'VistaSP2x64',
-                                'VistaSP2x86',
-                                'Win2003SP0x86',
-                                'Win2003SP1x64',
-                                'Win2003SP1x86',
-                                'Win2003SP2x64',
-                                'Win2003SP2x86',
-                                'Win2008R2SP0x64',
-                                'Win2008R2SP1x64',
-                                'Win2008SP1x64',
-                                'Win2008SP1x86',
-                                'Win2008SP2x64',
-                                'Win2008SP2x86',
-                                'Win2012R2x64',
-                                'Win2012x64',
-                                'Win7SP0x64',
-                                'Win7SP0x86',
-                                'Win7SP1x64',
-                                'Win7SP1x86',
-                                'Win8SP0x64',
-                                'Win8SP0x86',
-                                'Win8SP1x64',
-                                'Win8SP1x86',
-                                'WinXPSP1x64',
-                                'WinXPSP2x64',
-                                'WinXPSP2x86',
-                                'WinXPSP3x86']
-                                
-        if args.profile:
-            if args.profile in self.all_profiles:
-                self.profile = args.profile
-                print '[{0}] Profile {1} provided'.format(self.basename, self.profile)
-            else:
-                print '[{0}] Invalid profile {1} selected'.format(self.basename, args.profile)
-                sys.exit()
-                
-        if args.kdbgoffset:
-            self.kdbg = args.kdbgoffset
-            print '[{0}] KDBG offset {1} provided'.format(self.basename, self.kdbg)
-  
-        if not self.profile or not self.kdbg:
-            self.Identify(image)
-           
-        if args.readlist:
-            self.plugins = ReadPlugins(args.readlist)
-        else:
-            self.plugins = self.SelectValidPlugins()
-     
-        self.ProcessImage()
- 
-    def Identify(self, image):
-        
-        output_filename = 'imageinfo_' +  self.basename
-            
-        output_path = os.path.join(self.output, output_filename)
-           
-        print '[{0}] Running Plugin: imageinfo'.format(self.basename)
-        
-        data = subprocess.check_output(['vol.py', '-f', image, 'imageinfo'])
-        
-        try:
-            with open(output_path, 'w') as output:
-                output.write(data)
-        except:
-            print '[{0}] Error writing imageinfo'.format(self.basename)
-            
-        profiles_regex = re.search('Suggested Profile\(s\) : ([^\n]*)',  data)
-        profiles = profiles_regex.group(1).split(', ')
-        
-        kdbg_regex = re.search('KDBG : ([^\n]*)', data)
-        kdbg = kdbg_regex.group(1)
+	def __init__(self, invocation, image_path, profile, kdbg, master_output_directory, plugins_list):
+		self.invocation = invocation
+		self.basename = os.path.basename(image_path)
+		self.abspath = os.path.abspath(image_path)
+		self.output_directory = os.path.join(master_output_directory, self.basename)
+		self.profile = profile
+		self.kdbg = kdbg
+		self.valid_plugins = []
 
-        if not self.profile:
-            self.profile = profiles[0]
-        if not self.kdbg:
-            self.kdbg = kdbg
+		if not os.path.exists(self.output_directory):
+			os.makedirs(self.output_directory)  
+		    
+		if self.profile:
+			if not self.profile in ALL_PROFILES:
+				logging.error('[{0}] Invalid profile {1} selected'.format(self.basename, args.profile))
+				sys.exit()
 
-    def ReadPlugins(self, plugin_list):
-        print '[{0}] List of plugins provided by {1}'.format(self.basename, os.path.abspath(plugin_list))
-        
-        plugins = []
-        
-        try:
-            with open(plugin_file, 'r') as file:
-                for line in file:
-                 plugins.append(file.readline())
-        except:
-            print '[{0}] Error Reading Plugin File {1}'.format(self.basename, os.path.abspath(plugin_file))
-        
-        return plugins
-        
-    def SelectValidPlugins(self):
-        all =   ['pslist',
-                    'pstree',
-                 	'psscan',
-                	'dlllist',
-                	'handles',
-                	'getsids',
-                	'cmdscan',
-                	'consoles',
-                	'privs',
-                	'envars',
-                	'verinfo',
-                	'enumfunc',
-                	'memmap',
-                	'vadinfo',
-                	'vadwalk',
-                	'vadtree',
-                	'iehistory',
-                	'modules',
-                	'modscan',
-                    'ssdt',
-                  	'driverscan',
-                	'filescan',
-                	'mutantscan',
-                	'symlinkscan',
-                	'thrdscan',
-                	'unloadedmodules',
-                	'hivescan',
-                	'hivelist',
-                	'psxview',
-                	'malfind',
-                	'sessions',
-                	'wndscan',
-                	'deskscan',
-                	'atomscan',
-                	'clipboard',
-                	'eventhooks',
-                	'gahti',
-                	'messagehooks',
-                	'userhandles',
-                	'gditimers',
-                	'windows',
-                	'wintree',
-                	'svcscan',
-                	'ldrmodules',
-                	'apihooks',
-                	'idt',
-                	'gdt',
-                	'threads',
-                	'callbacks',
-                	'devicetree',
-                	'timers']
-        xp2003 = ['evtlogs',
-                    'connections',
-                    'connscan',
-                    'sockets',
-                    'sockscan']
-        vista2008win7 = ['netscan',
-                    'userassist',
-                    'shellbags',
-                    'shimcache',
-                    'getservicesids']
-                        	
-        OSType = re.match('(WinXP)|(Win2003)', self.profile)
-                
-        if OSType is not None:
-            plugins = all + xp2003
-            
-            for plugin in plugins:
-                print '[{0}] Loading valid plugin: {1}'.format(self.basename, plugin)
-                
-            return plugins
-        else:
-            plugins = all + vista2008win7
-            
-            for plugin in plugins:
-                print '[{0}] Loading valid plugin: {1}'.format(self.basename, plugin)
-                
-            return plugins
-            
-    def ProcessImage(self):
-        
-        if not os.path.exists(self.output):
-            os.makedirs(self.output)
-            
-        for plugin in self.plugins:
-            output_filename = plugin + '_' +  self.basename
-            
-            output_path = os.path.join(self.output, output_filename)
-            
-            print '[{0}] Running Plugin: {1}'.format(self.basename, plugin)
-            try:
-                with open(output_path, 'w') as output:
-                    subprocess.call(['vol.py', '-f', self.abspath, '--profile=' + self.profile, '--kdbg=' + self.kdbg, plugin], stderr=subprocess.STDOUT, stdout=output)
-                print '[{0}] {1} output saved to {2}'.format(self.basename, plugin, output_path)
-            except:
-                '[{0}] Error Running Plugin: {1}'.format(self.basename, plugin)
-        
-        print '[{0}] Processing complete. Exiting Gracefully'.format(self.basename)
+		if not self.profile or not self.kdbg:
+			output_filename = 'imageinfo_' +  self.basename
+			output_path = os.path.join(self.output_directory, output_filename)
+
+			data = subprocess.check_output([self.invocation, '-f', self.abspath, 'imageinfo'])
+
+			with open(output_path, 'w') as output:
+				output.write(data)
+
+			profiles_regex = re.search('Suggested Profile\(s\) : ([^\n]*)',  data)
+			auto_profiles = profiles_regex.group(1).split(', ')
+
+			kdbg_regex = re.search('KDBG : ([^\n]*)', data)
+			auto_kdbg = kdbg_regex.group(1)
+
+		if not self.profile:
+			self.profile = auto_profiles[0]
+		
+
+		if not self.kdbg:
+			self.kdbg = auto_kdbg
+
+		if plugins_list:
+			with open(plugins_list, 'r') as ifile:
+				for line in ifile:
+					self.valid_plugins.append(line)
+		else:
+			OSType = re.match('(WinXP)|(Win2003)', self.profile)
+			if OSType is not None:
+				self.valid_plugins = BASE_PLUGINS + XP2003_PLUGINS
+			else:
+				self.valid_plugins = BASE_PLUGINS + VISTA_WIN2008_WIN7_PLUGINS
+	
+		logging.info('[{0}] Selected Profile: {1}'.format(self.basename, self.profile))
+		logging.info('[{0}] Selected KDBG Offset: {1}'.format(self.basename, self.kdbg))
+		for plugin in self.valid_plugins:
+			logging.info('[{0}] Queuing plugin: {1}'.format(self.basename, plugin.strip('\n')))
+
+
+def execute_plugin(command):
+	logging.info('[{0}] Running Plugin: {1}'.format(command['image_basename'],
+		command['plugin_name']))
+
+	with open(command['output_path'], 'w') as output:
+		subprocess.call(command['commandline'], stderr=subprocess.STDOUT, stdout=output)
+
+	logging.info('[{0}] Plugin {1} output saved to {2}'.format(command['image_basename'], 
+		command['plugin_name'], command['output_path']))
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run all available Volatility plugins on a target image file.',
-        epilog='''The first suggested profile will be automatically selected.
-            All available plugins will be selected for the suggested profile.
-            If the output directory does not exist, it will be created.
-            The output files with follow a $plugin_$filename format.''')
-    parser.add_argument('--readlist', help='Flag to read from a list of plugins rather than using the default')
-    parser.add_argument('--profile', help='Provide a valid profile and bypass auto-detection')
-    parser.add_argument('--kdbgoffset', help='Provide a valid kdbg offset and bypass auto-detection')
-    parser.add_argument('--quick', help='Skips long-running plugins, like MFTparser')
-    parser.add_argument('outputdirectory', help='Path to output direcctory')
-    parser.add_argument('imagefiles', help='Path to memory image(s)', nargs='+')
-    args = parser.parse_args()
-    
-    output_directory    = os.path.abspath(args.outputdirectory)
-    image_files         = args.imagefiles
-    
-    print 'Bulk Volatility Scanner v0.4 - Ryan Cobb 01/09/2015'
-    
-    worker_processes = []
-    
-    for image in image_files:
-        process = multiprocessing.Process(target=MemoryImage, args=(image,))
-        worker_processes.append(process)
-        process.start()
+	parser = argparse.ArgumentParser(description='Run all available Volatility plugins on a target image file.',
+		epilog='''The first suggested profile will be automatically selected.
+			All available plugins will be selected for the suggested profile.
+			If the output directory does not exist, it will be created.
+			The output files with follow a $plugin_$filename format.''')
+	parser.add_argument('--invocation', help='Provide the desired invocation to execute Volatility')
+	parser.add_argument('--readlist', help='Flag to read from a list of plugins rather than using the default')
+	parser.add_argument('--profile', help='Provide a valid profile and bypass auto-detection')
+	parser.add_argument('--kdbgoffset', help='Provide a valid kdbg offset and bypass auto-detection')
+	parser.add_argument('output_directory', help='Path to output direcctory')
+	parser.add_argument('imagefiles', help='Path to memory image(s)', nargs='+')
+	args = parser.parse_args()
+
+	if args.invocation:
+		invocation = args.invocation
+	else:
+		invocation = 'vol.exe'
+
+	master_output_directory = os.path.abspath(args.output_directory)
+
+	if not os.path.exists(master_output_directory):
+		os.makedirs(master_output_directory)
+
+	profile = args.profile
+	kdbg = args.kdbgoffset
+	plugins_list = args.readlist
+	commands = []
+
+	for image_path in args.imagefiles:
+
+		image = MemoryImage(invocation, image_path, profile, kdbg, 
+			master_output_directory, plugins_list)
+
+		for plugin in image.valid_plugins:
+			if len(plugin.split(' ')) > 1:
+				plugin_name = plugin.split(' ')[0].strip('\n')
+				plugin_flags = [arg.strip('\n') for arg in plugin.split(' ')[1:]]
+			else:
+				plugin_name = plugin.strip('\n')
+				plugin_flags = []
+
+			output_filename = plugin_name + '_' +  image.basename + '.txt'
+			output_path = os.path.join(image.output_directory, output_filename)
+
+			commandline = [invocation, '-f', image.abspath, '--profile=' + profile, '--kdbg=' + image.kdbg, plugin_name]
+			commandline += plugin_flags
+
+			commands.append(
+				{
+				'image_basename': image.basename, 
+				'plugin_name': plugin_name, 
+				'commandline': commandline, 
+				'output_path': output_path
+				})
+
+
+	workers = []
+
+	while True:
+		logging.debug('Active Workers: {0}, Pending Tasks: {1}'.format(
+			len(workers), len(commands)))
+
+		if len(commands) == 0 and len(workers) == 0:
+			break
+
+		try:
+			if len(commands) > 0 and len(workers) < 4:
+				command = commands.pop()
+				worker = multiprocessing.Process(target=execute_plugin, args=(command,))
+				workers.append(
+					{'plugin_name': command['plugin_name'], 
+					'task': worker,
+					'image_basename': command['image_basename']})
+				worker.start()
+			else:
+				time.sleep(5)
+				logging.debug('Polling workers....')
+
+				for i, worker in enumerate(workers):
+					logging.debug('[{0}] Worker for {1} is still alive?: {2}'.format(
+						worker['image_basename'], worker['plugin_name'], worker['task'].is_alive()))
+					if not worker['task'].is_alive():
+						logging.debug('[{0}] Terminating finished worker for {1}'.format(
+							worker['image_basename'], worker['plugin_name']))
+						workers.pop(i)
+					else:
+						continue
+						
+		except KeyboardInterrupt:
+			for worker in workers:
+				worker['task'].terminate()
+			break
+
+	logging.info('Processing complete. Exiting gracefully.')
+	sys.exit()
